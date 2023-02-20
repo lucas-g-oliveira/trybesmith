8b6a9bb19ce0766b7386d90325c8dad818c598fc
Lucas G <lucasg.devmail93@gmail.com>
2023-02-20 19:51:34 -0300

    updated model Product

--- src/models/ProductModel.ts
+++ src/models/ProductModel.ts
@@ -9,21 +9,51 @@ export default class ProductModel implements IModelAddAndGetAll {
     this.connection = connection;
   }
 
+  // getAll
   public async getAll(): Promise<ProductType[]> {
     const result = await this.connection.execute('SELECT * FROM Trybesmith.products');
     const [rows] = result;
     return rows as ProductType[];
   }
 
+  // getById
+  public async getById(id:number):Promise<ProductType> {
+    const [result] = await this.connection
+      .execute('SELECT * FROM trybesmith.producs WHERE id = ?', [id]);
+    const [data] = result as ProductType[];
+    return data as ProductType;
+  }
+
+  // add
   public async add(product: ProductType): Promise<ProductType> {
     const { name, amount } = product;
-    const result = await this.connection.execute<ResultSetHeader>(
+    const [dataInserted] = await this.connection.execute<ResultSetHeader>(
       'INSERT INTO Trybesmith.products(name, amount) VALUES (?, ?)',
       [name, amount],
     );
-
-    const [dataInserted] = result;
     const { insertId } = dataInserted;
     return { id: insertId, ...product };
+  }
+
+  // addPlus
+  public async addWithOrderId(product:ProductType):Promise<ProductType> {
+    const arr = Object.keys(product);
+    const key = arr.join(', ');
+    const char = arr.map(() => '?').join(', ');
+    const result = await this.connection
+      .execute<ResultSetHeader>(`INSERT INTO Trybesmith.product (${key}) VALUES (${char})`, arr);
+
+    const [data] = result;
+    return { ...product, id: data.insertId } as ProductType;
+  }
+
+  // set
+  public async update(idOrder:number, idProduct:number):Promise<ProductType> {
+    await this.connection.execute<ResultSetHeader>(
+      'UPDATE Trybesmith.products SET order_id WHERE id = ?',
+      [idOrder, idProduct],
+    );
+    const devol = await this.getById(idProduct);
+    return devol;
   }
 }
